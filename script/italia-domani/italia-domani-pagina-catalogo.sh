92360943acf74e9f3884d91717941d0401ca4821
aborruso <aborruso@gmail.com>
2023-09-27 22:05:11 +0200

    update

--- script/italia-domani/italia-domani-pagina-catalogo.sh
+++ script/italia-domani/italia-domani-pagina-catalogo.sh
@@ -34,6 +34,7 @@ for (( i=1; i<=ultima; i++ )); do
 done
 
 # converte in HTML
+sort "$folder"/tmp.txt | uniq >"$folder"/lista.txt
 sort "$folder"/tmp.txt | uniq | sed 's|.*|- <&>|' | \
 pandoc -f markdown -s -t html --metadata title="catalogo opendata italiadomani" >"$folder"/../../data/italia-domani/web/catalogo-opendata.html
 
