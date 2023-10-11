3c089ced68e344112a22b317b84531de0f799f1f
aborruso <aborruso@gmail.com>
2023-10-11 11:55:43 +0200

    archivio lista file all-time. Più comodo per rss update

--- script/italia-domani/italia-domani-pagina-catalogo.sh
+++ script/italia-domani/italia-domani-pagina-catalogo.sh
@@ -38,6 +38,9 @@ done
 
 # converte in HTML
 sort "$folder"/tmp.txt | sort | uniq >"$folder"/lista.txt
+sort "$folder"/tmp.txt | sort | uniq >>"$folder"/lista_all_time.txt
+sort -u "$folder"/lista_all_time.txt >"$folder"/lista_all_time.txt.tmp
+mv "$folder"/lista_all_time.txt.tmp "$folder"/lista_all_time.txt
 sort "$folder"/tmp.txt | sort | uniq | sed 's|.*|- <&>|' | \
 pandoc -f markdown -s -t html --metadata title="catalogo opendata italiadomani" >"$folder"/../../data/italia-domani/web/catalogo-opendata.html
 
