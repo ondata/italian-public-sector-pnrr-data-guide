7182f0f34396e79ca899932e90fe41b83f09dd40
aborruso <aborruso@gmail.com>
2025-01-01 09:08:03 +0100

    update

--- script/italia-domani/italia-domani_parquet.sh
+++ script/italia-domani/italia-domani_parquet.sh
@@ -17,36 +17,41 @@ trap "rm -f '${folder}/tmp/tmp.csv'" EXIT
 <"${folder}/lista.txt" grep '.csv' | grep -vP "(_v|\d)" | while IFS= read -r url; do
     nome=$(basename "${url}" .csv | tr ' ' '_')
 
-    # Scarica il file
-    curl -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36' \
-        -skL "${url}" > "${folder}/tmp/tmp.csv"
+    # se non esiste scarica il file
+    if [ ! -f "${folder}/../../data/italia-domani/parquet/${nome}.parquet" ]; then
 
-    # se il nome del file è "PNRR_Soggetti" scrivi "evviva"
-    if [[ "${nome}" == "PNRR_Soggetti" ]]; then
-        awk -F';' 'NR == 1 || NF == 12 {print}' "${folder}/tmp/tmp.csv" > "${folder}/tmp/tmp_ok.csv"
-        mv "${folder}/tmp/tmp_ok.csv" "${folder}/tmp/tmp.csv"
+        # Scarica il file
+        curl -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36' \
+            -skL "${url}" >"${folder}/tmp/tmp.csv"
+
+        # se il nome del file è "PNRR_Soggetti" scrivi "evviva"
+        if [[ "${nome}" == "PNRR_Soggetti" ]]; then
+            awk -F';' 'NR == 1 || NF == 12 {print}' "${folder}/tmp/tmp.csv" >"${folder}/tmp/tmp_ok.csv"
+            mv "${folder}/tmp/tmp_ok.csv" "${folder}/tmp/tmp.csv"
+        fi
+
+        # Controlla la codifica del file
+        charset=$(chardetect --minimal "${folder}/tmp/tmp.csv" | tr '[:lower:]' '[:upper:]')
+
+        # Se MACROMAN, modificalo in ISO-8859-1
+        if [[ "${charset}" == "MACROMAN" ]]; then
+            charset="ISO-8859-1"
+        fi
+
+        # Verifica e converte in UTF-8 se necessario
+        if [[ -z "${charset}" ]]; then
+            echo "Errore: impossibile rilevare la codifica di ${url}" >&2
+            continue
+        elif [[ "${charset}" != "UTF-8" && "${charset}" != "UTF-8-SIG" ]]; then
+            mv "${folder}/tmp/tmp.csv" "${folder}/tmp/tmp_original.csv"
+            iconv -f "${charset}" -t UTF-8 "${folder}/tmp/tmp_original.csv" -o "${folder}/tmp/tmp.csv"
+            rm -f "${folder}/tmp/tmp_original.csv"
+        fi
+
+        # Converte il file CSV in Parquet
+        duckdb -c "COPY (SELECT * FROM read_csv_auto('${folder}/tmp/tmp.csv',decimal_separator=',',sample_size=-1)) TO '${folder}/../../data/italia-domani/parquet/${nome}.parquet' (FORMAT 'parquet', CODEC 'ZSTD')"
     fi
 
-    # Controlla la codifica del file
-    charset=$(chardetect --minimal "${folder}/tmp/tmp.csv" | tr '[:lower:]' '[:upper:]')
-
-    # Se MACROMAN, modificalo in ISO-8859-1
-    if [[ "${charset}" == "MACROMAN" ]]; then
-        charset="ISO-8859-1"
-    fi
-
-    # Verifica e converte in UTF-8 se necessario
-    if [[ -z "${charset}" ]]; then
-        echo "Errore: impossibile rilevare la codifica di ${url}" >&2
-        continue
-    elif [[ "${charset}" != "UTF-8" && "${charset}" != "UTF-8-SIG" ]]; then
-        mv "${folder}/tmp/tmp.csv" "${folder}/tmp/tmp_original.csv"
-        iconv -f "${charset}" -t UTF-8 "${folder}/tmp/tmp_original.csv" -o "${folder}/tmp/tmp.csv"
-        rm -f "${folder}/tmp/tmp_original.csv"
-    fi
-
-    # Converte il file CSV in Parquet
-    duckdb -c "COPY (SELECT * FROM read_csv_auto('${folder}/tmp/tmp.csv',decimal_separator=',',sample_size=-1)) TO '${folder}/../../data/italia-domani/parquet/${nome}.parquet' (FORMAT 'parquet', CODEC 'ZSTD')"
 done
 
 # Pulizia finale del file temporaneo
