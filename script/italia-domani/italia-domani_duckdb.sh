af524431e8edef014cfbe203d18806d9a6df4ca7
aborruso <aborruso@gmail.com>
2025-01-01 13:21:18 +0100

    aggiunta tabella info su tabelle

--- script/italia-domani/italia-domani_duckdb.sh
+++ script/italia-domani/italia-domani_duckdb.sh
@@ -7,14 +7,24 @@ set -o pipefail
 
 folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-mkdir -p "${folder}/tmp"
+mkdir -p "${folder}"/tmp
 
 if [ -f "${folder}"/../../data/italia-domani/parquet/pnrr.db ]; then
     rm "${folder}"/../../data/italia-domani/parquet/pnrr.db
 fi
 
-find "${folder}"/../../data/italia-domani/parquet -name "*.parquet" | while read -r file; do
+mlr --ijsonl --onidx cut -f file_url then put '$file_url=sub($file_url,"^(.+/)(.+)(\..+)$","\2.")' "${folder}"/../../data/italia-domani/lista_full.jsonl >"${folder}"/tmp/tmp.txt
+
+find "${folder}"/../../data/italia-domani/parquet -name "*.parquet" | grep -Ff tmp.txt | while read -r file; do
     table=$(basename "${file}" .parquet)
     duckdb "${folder}"/../../data/italia-domani/parquet/pnrr.db -c "create or replace view \"${table}\" AS
 select * from read_parquet('https://raw.githubusercontent.com/ondata/italian-public-sector-pnrr-data-guide/refs/heads/main/data/italia-domani/parquet/${table}.parquet')"
 done
+
+find "${folder}"/../../data/italia-domani/parquet -name "*.parquet" | grep -Ff tmp.txt | mlr --inidx --ojsonl label file then put '$file=sub($file,"^(.+/)(.+)(\..+)$","\2.csv")' >"${folder}"/tmp/tmp.jsonl
+
+mlr --jsonl join -j file -f "${folder}"/tmp/tmp.jsonl then unsparsify "${folder}"/../../data/italia-domani/lista_full.jsonl >"${folder}"/tmp/tmp2.jsonl
+
+mlr --jsonl --from "${folder}"/tmp/tmp2.jsonl rename file,table,file_url,table_source then put '$table=sub($table,"\..+","")' then reorder -f table,title,page_url,table_source then put '$data_osservazione=strftime(strptime($data_osservazione, "%d/%m/%y"),"%Y-%m-%d")' then sort -t table then clean-whitespace >"${folder}"/tmp/info_tabelle.jsonl
+
+duckdb "${folder}"/../../data/italia-domani/parquet/pnrr.db -c "create table info_tabelle as select * from read_ndjson('${folder}/tmp/info_tabelle.jsonl')"
